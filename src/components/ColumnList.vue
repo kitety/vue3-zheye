df526bcab2c8a76c4c2cdd432acc5342f24ea4a4
kitety <aizaizuori@foxmail.com>
2021-02-02 23:03:34 +0800

    feat: add header

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm" style="width: 18rem;">
+        <div class="card-body text-center">
+          <img :src="column.avatar" class="round-circle border border-light w-25 my-3" :alt="column.title" />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-start">
+            {{ column.description }}
+          </p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar?: string;
+  description: string;
+}
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(col => {
+        if (!col.avatar) {
+          col.avatar = require("@/assets/pig.jpg");
+        }
+        return col;
+      });
+    });
+    return { columnList };
+  }
+});
+</script>
