c23ade8b96fa0a1fa77d88f13b7b5eadb6aa85a9
kitety <aizaizuori@foxmail.com>
2021-02-07 22:11:00 +0800

    feat: header dropdown

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dropdown" ref="dropdownRef">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, watch } from "vue";
+import useClickOutside from "../hooks/useClickOutside";
+export default defineComponent({
+  name: "Dropdown",
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const isOpen = ref(false);
+    const dropdownRef = ref<null | HTMLElement>(null);
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value;
+    };
+
+    const isclickOutside = useClickOutside(dropdownRef);
+
+    watch(isclickOutside, () => {
+      if (isclickOutside.value && isOpen.value) {
+        toggleOpen();
+      }
+    });
+    return { isOpen, toggleOpen, dropdownRef };
+  }
+});
+</script>
